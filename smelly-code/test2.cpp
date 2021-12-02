e32a89d229e1872785e7286c799cfa43f96b67f8
Erik Tran <35762899+eriktran@users.noreply.github.com>
2021-12-02 17:33:12 +0100

    Added test2.cpp

---
+++ smelly-code/test2.cpp
@@ -0,0 +1,21 @@
+void comments() {
+    int x = 10;
+    int a;
+    
+    /*
+    i nt wh ile;
+    cholecystokinin thyroxine
+    voi d ouble
+    i for
+    if or
+    i f or
+    i f o r
+    */
+
+    /*
+    int a
+    /* int b
+    // int c
+    int e
+    */
+}
